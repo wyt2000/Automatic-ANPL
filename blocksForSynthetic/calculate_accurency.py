84dc371cb3e27a86c7341091a0a43fdb12a4ae5e
wyt2000 <wytwyt200@gmail.com>
2023-08-10 11:54:34 +0800

    fix: add re to accurency.

--- blocksForSynthetic/calculate_accurency.py
+++ blocksForSynthetic/calculate_accurency.py
@@ -1,7 +1,7 @@
 import json
 import pathlib
 
-paths = pathlib.Path(".").glob("anpl*.json")
+paths = pathlib.Path(".").glob("parsel*.json")
 paths = sorted(paths)
 for path in paths:
     with open(path, 'r') as f:
@@ -9,8 +9,9 @@ for path in paths:
     block_num = int(path.stem.split("_")[-1])
     name = data["compiler_name"]
     ce = len(data["compile_errors"])
-    wa = len(data["wrong_answers"]) 
+    wa = len(data["wrong_answers"])
     tle = len(data["time_limit_exceededs"])
+    re = len(data["runtime_errors"])
     ac = len(data["accepteds"])
-    print(f"[{name}]\tlength:{block_num}\tac:{ac}\twa:{wa}\tce:{ce}\ttle:{tle}\tacc:{ac / (ac+wa+ce+tle):.4f}")
+    print(f"[{name:^10}]\tlength:{block_num}\tac:{ac}\twa:{wa}\tce:{ce}\tre:{20-ac-wa-ce-tle}\ttle:{tle}\tacc:{ac / (20):.4f}")
 

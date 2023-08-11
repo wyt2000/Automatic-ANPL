3146330313a4c859d20162f370e5bd73564195c9
wyt2000 <wytwyt200@gmail.com>
2023-08-11 11:35:27 +0800

    refactor: ProgramBuilder -> ProgramSampler.

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-from ProgramBuilder import ProgramBuilder
-from GPT2Code import GPT2Code 
-from ANPLCompiler import ANPLCompiler
-from ParselCompiler import ParselCompiler
-from ParselPrompts import background as parsel_background
-from ParselPrompts import pre_prompt as parsel_pre_prompt
-from ParselPrompts import post_prompt as parsel_post_prompt
-from ParselPrompts import extract_code as parsel_extract_code
 import os
 import pathlib
 import importlib
@@ -13,6 +5,16 @@ import json
 import dataclasses
 import timeout_decorator
 import traceback
+
+from ProgramSampler import ProgramSampler 
+from GPT2Code import GPT2Code 
+from ANPLCompiler import ANPLCompiler
+from ParselCompiler import ParselCompiler
+from ParselPrompts import background as parsel_background
+from ParselPrompts import pre_prompt as parsel_pre_prompt
+from ParselPrompts import post_prompt as parsel_post_prompt
+from ParselPrompts import extract_code as parsel_extract_code
+from utils import mkdir_override
 
 time_limit = 10
 
@@ -26,15 +28,15 @@ class CompileInfo:
     wrong_answers : dict[str, int] = dataclasses.field(default_factory=dict) 
     accepteds : dict[str, int] = dataclasses.field(default_factory=dict) 
 
-def test_compiler(builder, compiler, robot, model_name, prompt_dir, response_dir, result_dir, compile_info_path):
+def test_compiler(sampler, compiler, robot, model_name, prompt_dir, response_dir, result_dir, compile_info_path):
     try:
-        builder.mkdir_override(response_dir)
-        builder.mkdir_override(result_dir)
+        mkdir_override(response_dir)
+        mkdir_override(result_dir)
         compile_info = CompileInfo(compiler.name)
 
-        for i, data in enumerate(builder.dataset):
+        for i, data in enumerate(sampler.dataset):
             try:
-                task_name = f"{compiler.name}_{data.name}"
+                task_name = f"{compiler.name}_{data.prog_name}"
                 print(f'{task_name}: requesting for {model_name}...')
                 response = robot.request(model_name,
                                          data.func_name,
@@ -43,7 +45,7 @@ def test_compiler(builder, compiler, robot, model_name, prompt_dir, response_dir
                 print(f'{task_name} request for {model_name} done!, the response {compiler.name} code is:\n{response}')
                 code_path = os.path.join(result_dir, f"{task_name}.py")
                 try:
-                    code = compiler.compile(data.name, response, code_path)
+                    code = compiler.compile(data.prog_name, response, code_path)
                 except Exception as err:
                     print(f'{task_name}: synthesis failed!')
                     traceback.print_exc()
@@ -52,7 +54,7 @@ def test_compiler(builder, compiler, robot, model_name, prompt_dir, response_dir
                     pass
                 if code is None:
                     print(f'{task_name}: compile error!')
-                    compile_info.compile_errors[data.name] = data.block_num
+                    compile_info.compile_errors[data.prog_name] = data.num_snippets
                     continue
 
                 try:
@@ -62,7 +64,7 @@ def test_compiler(builder, compiler, robot, model_name, prompt_dir, response_dir
                 except:
                     print(f'{task_name}: func {data.func_name} not found, compile error!')
                     traceback.print_exc()
-                    compile_info.compile_errors[data.name] = data.block_num
+                    compile_info.compile_errors[data.prog_name] = data.num_snippets
                     continue
                 @timeout_decorator.timeout(time_limit)
                 def timeout_func(inp):
@@ -75,21 +77,21 @@ def test_compiler(builder, compiler, robot, model_name, prompt_dir, response_dir
                     except timeout_decorator.TimeoutError as err:
                         print(f'{task_name}: Time limit exceeded at {data.func_name}(\"{inp}\") = \"{ans}\"!')
                         ok = False
-                        compile_info.time_limit_exceededs[data.name] = data.block_num
+                        compile_info.time_limit_exceededs[data.prog_name] = data.num_snippets
                         break
                     except Exception as err:
                         print(f'{task_name}: Runtime error at {data.func_name}(\"{inp}\") = \"{ans}\"!')
                         ok = False
-                        compile_info.runtime_errors[data.name] = data.block_num
+                        compile_info.runtime_errors[data.prog_name] = data.num_snippets
                         break
                     if out != ans: 
                         print(f'{task_name}: Wrong Answer! {data.func_name}(\"{inp}\") should be \"{ans}\"!')
                         ok = False
-                        compile_info.wrong_answers[data.name] = data.block_num
+                        compile_info.wrong_answers[data.prog_name] = data.num_snippets
                         break
                 if ok:
                     print(f'{task_name}: Accepted!')
-                    compile_info.accepteds[data.name] = data.block_num
+                    compile_info.accepteds[data.prog_name] = data.num_snippets
             except Exception as err:
                 print(err)
     finally:
@@ -98,14 +100,14 @@ def test_compiler(builder, compiler, robot, model_name, prompt_dir, response_dir
 
 if __name__ == '__main__':
 
-    for block_num in range(1, 8):
-        builder = ProgramBuilder()
-        builder.build(
-            block_num=block_num,
-            output_dir=f'programs_{block_num}/',
-            output_prefix=f'string_manipulation_{block_num}',
-            prompt_dir=f'prompts_{block_num}/',
-            seed=int(f'{block_num}114514')
+    for num_snippets in range(1, 8):
+        sampler = ProgramSampler()
+        sampler.sample(
+            num_snippets=num_snippets,
+            program_dir=f'programs_{num_snippets}/',
+            program_prefix=f'string_manipulation_{num_snippets}',
+            prompt_dir=f'prompts_{num_snippets}/',
+            seed=int(f'{num_snippets}114514')
         )
        
         
@@ -113,14 +115,14 @@ if __name__ == '__main__':
         anpl_compiler = ANPLCompiler(max_try_times=5, max_temperature=0.5)
 
         test_compiler(
-            builder=builder,
+            sampler=sampler,
             compiler=anpl_compiler,
             robot=anpl_robot,
             model_name='gpt-3.5-turbo-0301',
-            prompt_dir=f'prompts_{block_num}/',
-            response_dir=f'anpl_responses_{block_num}/',
-            result_dir=f'anpl_results_{block_num}/',
-            compile_info_path=f'anpl_compile_info_{block_num}.json',
+            prompt_dir=f'prompts_{num_snippets}/',
+            response_dir=f'anpl_responses_{num_snippets}/',
+            result_dir=f'anpl_results_{num_snippets}/',
+            compile_info_path=f'anpl_compile_info_{num_snippets}.json',
         )
         
 
@@ -129,14 +131,14 @@ if __name__ == '__main__':
         parsel_compiler = ParselCompiler()
 
         test_compiler(
-            builder=builder,
+            sampler=sampler,
             compiler=parsel_compiler,
             robot=parsel_robot,
             model_name='gpt-3.5-turbo-0301',
-            prompt_dir=f'prompts_{block_num}/',
-            response_dir=f'parsel_responses_{block_num}/',
-            result_dir=f'parsel_results_{block_num}/',
-            compile_info_path=f'parsel_compile_info_{block_num}.json',
+            prompt_dir=f'prompts_{num_snippets}/',
+            response_dir=f'parsel_responses_{num_snippets}/',
+            result_dir=f'parsel_results_{num_snippets}/',
+            compile_info_path=f'parsel_compile_info_{num_snippets}.json',
         )
 
 
--- utils.py
+++ utils.py
@@ -1,33 +1,9 @@
-import pickle
-import json
-import time
 import os
+import shutil
+import pathlib
 
-def read_io(path):
-    with open(path, "r") as f:
-        test = json.load(f)
-    return test
+def mkdir_override(dir_path):
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
+    pathlib.Path(dir_path).mkdir(parents=True)
 
-def read_anpl(path):
-    with open(path, "rb") as f:
-        anpl = pickle.load(f)
-    return anpl
-
-def clean_anpl(prev_anpl, new_anpl):
-    new_fun_names = set(new_anpl.funs.keys() - prev_anpl.funs.keys())
-    subfuns = set()
-    for name in new_fun_names:
-        if name in new_anpl.funs:
-            for n in new_anpl.funs[name].dependencies:
-                subfuns.add(n)
-    entries = new_fun_names - subfuns
-    if len(entries) == 1:
-        new_anpl.clean(entries.pop())
-        return True
-    else:
-        return False
-
-def save(anpl, save_path):
-    code = anpl.to_python()
-    with open(save_path, "w") as f:
-        f.write(code)

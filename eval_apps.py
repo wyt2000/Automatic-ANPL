f11d86d1b5f7092b91262d9ce3dfe8baf114a4f7
wyt2000 <wytwyt200@gmail.com>
2023-08-21 19:22:11 +0800

    fix: eval script.

--- eval_apps.py
+++ eval_apps.py
@@ -1,17 +1,17 @@
 import pathlib
 import json
-from evaluate import load
+from apps_metric.apps_metric import apps_metric
 
 def get_codes(target_dir, prefix, num_problems):
     generations = [None] * num_problems
     for i in range(num_problems):
         path = pathlib.Path(target_dir, f'{prefix}_{i}.json')
         with open(path, 'r') as f:
-            generations[i] = json.loads(f.read())
+            generations[i] = json.loads(f.read())[:20]
+    return generations
 
 if __name__ == '__main__':
     generations = get_codes('gpt_apps_code', 'gpt', 5)
-    apps_metric = load('codeparrot/apps_metric')
-    results = apps_metric.compute(predictions=generations, level="all")
+    results = apps_metric().compute(predictions=generations, k_list=[20], level="all")
     print(results)
 

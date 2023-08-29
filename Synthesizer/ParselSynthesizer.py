7b5a2fdf31df651afcde9b67536ce83bb695bcee
wyt2000 <wytwyt200@gmail.com>
2023-08-29 19:57:39 +0800

    feat: Add ParselSynthesizer.

--- Synthesizer/ParselSynthesizer.py
+++ Synthesizer/ParselSynthesizer.py
@@ -1,27 +1,47 @@
 import os
 import traceback
-from .parsel import parsel
-from .Synthesizer import AbstractSynthesizer
+from parsel import parsel, codex, graph
 
-class ParselSynthesizer(AbstractSynthesizer):
+_io_template = '''
+from sys import stdin
 
-    cache_path = 'cache.json'
-    key_path = 'key.txt'
+inp = ""
+for line in stdin:
+    inp += line
+print({root}(inp))
+'''
 
-    @property
-    def name(self):
-        return 'parsel'
+class ParselSynthesizer:
 
-    def synthesize(self, program, save_path, *args):
-        codegen = parsel.CodeGen(self.cache_path, self.key_path)
-        code = None
-        try:
-            code = parsel.parsel_str(codegen, program, target_file=save_path)
-        except Exception as err:
-            traceback.print_exc()
-            print(err)
-        finally:
-            if os.path.isfile(self.cache_path):
-                os.remove(self.cache_path)
-        return code
+    # TODO: handle starter code.
+    def transform(self, target_code, root):
+        return target_code + _io_template.format(root=root)
+
+    def synthesize(self,
+                   parsel_code: str,
+                   save_path_prefix: str,
+                   cache_path_prefix: str,
+                   num_completions_list: list[int] = [1]):
+
+        parsel_code = parsel_code.strip().splitlines()
+        _, defined_fns = parsel.get_graph(parsel_code)
+        root = graph.get_root(defined_fns)
+        results = {}
+        for num_completions in num_completions_list:
+            codeGen = codex.CodeGen(
+                cache = f'{cache_path_prefix}.json'
+            )
+            compiled_fns = parsel.parsel_graph(defined_fns, codeGen, num_completions=num_completions)
+            target_code = parsel.fns_to_str(defined_fns[root], set())
+            target_code = self.transform(target_code, root)
+            results[num_completions] = target_code
+            with open(f'{save_path_prefix}_{num_completions}.py', 'w') as f:
+                f.write(target_code)
+        return results
+
+if __name__ == '__main__':
+    parselSynthesizer = ParselSynthesizer()
+    with open('test.ss', 'r') as f:
+        parsel_code = f.read()
+    parselSynthesizer.synthesize(parsel_code, 'test/test', 'test/test', [1, 2, 4, 8, 16])
 

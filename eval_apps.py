535f0283caa0927a756eefce663c40bd231b0e7c
wyt2000 <wytwyt200@gmail.com>
2023-09-04 17:58:19 +0800

    feat: add args in eval_apps.

--- eval_apps.py
+++ eval_apps.py
@@ -3,6 +3,7 @@ from apps_metric.apps_metric import apps_metric
 import argparse
 
 def get_codes(target_dir: str,
+              start_problem_id: int,
               num_problems: int,
               num_codes: int,
               num_completions: int,
@@ -12,27 +13,25 @@ def get_codes(target_dir: str,
     for path in paths:
         name = path.stem.split('_')
         problem_id = int(name[1])
-        if problem_id - 3000 >= num_problems: continue
+        if problem_id - start_problem_id >= num_problems: continue
         code_id = int(name[2])
         with open(path, 'r') as f:
-            s = generations[problem_id - 3000][code_id]
-            generations[problem_id - 3000][code_id] = f.read()
+            s = generations[problem_id - start_problem_id][code_id]
+            generations[problem_id - start_problem_id][code_id] = f.read()
     return generations
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument("-t", "--target", help="Path of target code folder", type=str, required=True)
+    argparser.add_argument("-s", "--start", help="Start problem id", type=int, required=True)
     argparser.add_argument("-p", "--num_problems", help="Number of problems", type=int, required=True)
     argparser.add_argument("-n", "--num_codes", help="Number of code for each problem", type=int, required=True)
     argparser.add_argument("-k", "--num_completions", help="Number of function implementations for each code", type=int, required=True)
     args = argparser.parse_args()
 
-    generations = get_codes(args.target, args.num_problems, args.num_codes, args.num_completions)
-    results = apps_metric().compute(predictions=generations, k_list=[args.num_codes], level="competition")
+    generations = get_codes(args.target, args.start, args.num_problems, args.num_codes, args.num_completions)
+    generations = [[gen[1]] for gen in generations]
+    #print(generations)
+    results = apps_metric().compute(predictions=generations, k_list=[args.num_codes], level="introductory")
     print(results)
-
-    #for i in range(8):
-    #    gens = [[gen[i]] for gen in generations]
-    #    results = apps_metric().compute(predictions=gens, k_list=[args.num_codes], level="competition")
-    #    print(results)
 

086fd681e906b46de852284ed9fe6b7713823a12
wyt2000 <wytwyt200@gmail.com>
2023-09-11 15:37:19 +0800

    fix: sample without starter_code.

--- ProblemSampler/APPSProblemSampler.py
+++ ProblemSampler/APPSProblemSampler.py
@@ -1,7 +1,7 @@
 import json
 import random
 from dataclasses import dataclass
-from .ProblemSampler import ProblemData, ProblemSampler
+from ProblemSampler import ProblemData, ProblemSampler
 
 # for huggingface
 import datasets
@@ -28,6 +28,7 @@ class APPSProblemSampler(ProblemSampler):
     def __init__(self, path='codeparrot/apps', split='test', difficulties=['all']):
         datasets.config.DEFAULT_MAX_BATCH_SIZE = 10
         self.apps = load_dataset(path=path, split=split, difficulties=difficulties)
+        self.valid_ids = [i for i in range(len(self.apps)) if self.apps[i]["starter_code"] == ''] # not support starter_code now.
 
     def sample(self, data_indices):
         for idx in data_indices: 
@@ -38,11 +39,11 @@ class APPSProblemSampler(ProblemSampler):
 
     def sample_randomly(self, num_problems, seed=114514):
         random.seed(seed)
-        yield from self.sample(random.sample(range(len(self.apps)), num_problems))
+        yield from self.sample(random.sample(self.valid_ids, num_problems))
 
 if __name__ == '__main__':
-    sampler = APPSProblemSampler()
-    dataset = sampler.sample_from_head(10)
+    sampler = APPSProblemSampler(difficulties=['competition'])
+    dataset = sampler.sample_randomly(10)
     for data in dataset:
         print(data)
 

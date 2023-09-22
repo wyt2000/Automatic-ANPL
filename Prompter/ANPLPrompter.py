67146de70d87cbd222981ac03838140127b1c86c
wyt2000 <wytwyt200@gmail.com>
2023-09-22 17:33:47 +0800

    fix: update prompts.

--- Prompter/ANPLPrompter.py
+++ Prompter/ANPLPrompter.py
@@ -62,16 +62,25 @@ def main(input_str: str) -> str:
 
 # Here is an example of the format applied to identifying the winner of a game:
 ```
-def parse_input(input_str):
-    "Takes a string containing the length on the first line and the integers on the second and returns the list of integers"
-def all_gcd(l):
-    "Returns the greatest common divisor of all elements in the list"
-def num_moves(l):
-    "the number of moves is the largest element in the list divided by the greatest common divisor of all elements in the list, minus the length of the list.Return the the number of moves"
-def to_output_str(num_moves):
-    "Returns the string 'Alice' if the number of moves is odd and 'Bob' if the number of moves is even"
-def main(input_str):
-    "Returns the winner of the game."
+def parse_input(input_str: str) -> list[int]:
+    '''
+    Takes a string containing the length on the first line and the integers on the second and returns the list of integers.
+    '''
+
+def num_moves(l: list[int]) -> int:
+    '''
+    The number of moves is the largest element in the list divided by the greatest common divisor of all elements in the list, minus the length of the list. Return the the number of moves.
+    '''
+
+def to_output_str(num_moves: int) -> str:
+    '''
+    Returns the string 'Alice' if the number of moves is odd and 'Bob' if the number of moves is even.
+    '''
+
+def main(input_str: str) -> str:
+    '''
+    Returns the winner of the game.
+    '''
     input_list = parse_input(input_str)
     moves_count = num_moves(input_list)
     return to_output_str(moves_count)
@@ -80,19 +89,39 @@ def main(input_str):
 # Here is an example of the format applied to finding a biggest number according to some rules:
 ```
 def parse_input(input_str):
-    "Takes a string containing the number of towers on the first line and the heights of the towers on the second and returns the list of heights"
+    '''
+    Takes a string containing the number of towers on the first line and the heights of the towers on the second and returns the list of heights.
+    '''
+
 def side_ones(heights_list):
-    "From a list of ints, set the first and last elements to 1 and return the list"
+    '''
+    From a list of ints, set the first and last elements to 1 and return the list.
+    '''
+
 def destroy_from_left(side_list): 
-    "Copy the list and set each each element to the minimum of itself and one more than the element to its left, starting from the second element"
+    '''
+    Copy the list and set each each element to the minimum of itself and one more than the element to its left, starting from the second element.
+    '''
+
 def destroy_from_right(side_list):
-    "Copy the list and set each each element to the minimum of itself and one more than the element to its right, starting from the second to last element"
+    '''
+    Copy the list and set each each element to the minimum of itself and one more than the element to its right, starting from the second to last element.
+    '''
+
 def min_list(l1, l2):
-    "Return a list of the minimum of the corresponding elements of l1 and l2"
+    '''
+    Return a list of the minimum of the corresponding elements of l1 and l2.
+    '''
+
 def to_output_str(num_moves):
-    "Return the string representation of the maximum element in the list"
+    '''
+    Return the string representation of the maximum element in the list.
+    '''
+
 def main(input_str):
-    "Returns the winner of the game."
+    '''
+    Returns the winner of the game.
+    '''
     heights_list = parse_input(input_str)
     slist = side_ones(heights_list)
     l1 = destroy_from_left(slist)

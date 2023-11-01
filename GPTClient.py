4cc29c1d28f760bed0173c8ed6eaedceea83ffde
wyt2000 <wytwyt200@gmail.com>
2023-11-01 10:29:28 +0800

    fix: GPTClient.

--- GPTClient.py
+++ GPTClient.py
@@ -40,8 +40,12 @@ class GPTClient:
             except openai.error.InvalidRequestError as err:
                 self.logger.debug(f'{task_name}: InvalidRequestError!')
                 raise err
-            except Exception as err:
+            except openai.error.RateLimitError as err:
+                self.logger.exception(err)
                 await asyncio.sleep(self.retry_interval * (2 ** i))
+            except Exception as err:
+                self.logger.exception(err)
+                return []
         raise openai.error.RateLimitError
 
     # Convert GPT responses to list[str]
